ee1dbb42e4268cdc9d450e2ee6b6d5e87058b8de
DESKTOP-GGBBF12\moris <morishi0318@outlook.jp>
2023-07-12 23:55:16 +0900

    黒板表示系全部OK

--- Assets/script/Blackboard.cs
+++ Assets/script/Blackboard.cs
@@ -11,10 +11,44 @@ public class Blackboard : MonoBehaviour
 
     public GameObject _TextNode;/// 生成するtextPrefabのための変数宣言
     public GameObject _Text_parent;///textnodeの親要素用変数
+	public int maxChildCount = 3;
+	private LoadText _LoadText;
+
+
+	void Start()
+    {
+        ///LoadTextスクリプトにアクセスする用
+	_LoadText = GameObject.Find("LoadTextScript").GetComponent<LoadText>();
+    }
+
+	public void Text_Explain()
+	{
+		//　読み込んだテキストファイルの内容を表示
+			if (_LoadText.splitText1[_LoadText.textNum1] != "")
+			{
+				Create_TextNode(_LoadText.splitText1[_LoadText.textNum1]);
+				_LoadText.textNum1++;
+				if (_LoadText.textNum1 >= _LoadText.splitText1.Length)
+				{
+					_LoadText.textNum1 = 0;
+				}
+			}
+			else
+			{
+				Create_TextNode("これはエラーテキストです");
+				_LoadText.textNum1++;
+			}
+	}
+
 
     public void Create_TextNode(string text)
     {
-        ///テキストを黒板上に表示するためのメソッド
+        ///テキストを黒板上に表示するためのメソッド///
+
+        if (_Text_parent.transform.childCount >= maxChildCount)///黒板上に３つ以上Nodeが溜まったら削除する
+        {
+            Destroy_TextNode();
+        }
 
         GameObject instantiatedNode_Prefab = Instantiate(_TextNode, _Text_parent.transform);///Prefab生成
 
@@ -27,5 +61,13 @@ public class Blackboard : MonoBehaviour
         _blackboard_Node.text = text;
 
 
+    }
+
+    private void Destroy_TextNode()
+    {
+        foreach (Transform child in _Text_parent.transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
--- Assets/script/LoadText.cs
+++ Assets/script/LoadText.cs
@@ -9,27 +9,27 @@ public class LoadText : MonoBehaviour
 {
 
 	//　読み込んだテキストを出力するUIテキスト
-	[SerializeField]
-	private TextMeshProUGUI dataText;
+	///[SerializeField]
+	///public TextMeshProUGUI dataText;
 	//　読む込むテキストが書き込まれている.txtファイル
 	[SerializeField]
-	private TextAsset textAsset;
+	public TextAsset textAsset;
 	//　テキストファイルから読み込んだデータ
-	private string loadText1;
+	public string loadText1;
 	//　改行で分割して配列に入れる
-	private string[] splitText1;
+	public string[] splitText1;
 	//　現在表示中テキスト1番号
-	private int textNum1;
+	public int textNum1;
 
 	void Start()
 	{
-		loadText1 = textAsset.text;///指定したテキストアセットをloadText1に入れる
-		splitText1 = loadText1.Split(char.Parse("\n"));///改行で区切って配列型splitText1に入れる
+		loadText1 = textAsset.text;////指定したテキストアセットをloadText1に入れる
+		splitText1 = loadText1.Split(char.Parse("。"));///改行で区切って配列型splitText1に入れる
 		textNum1 = 0;
-		dataText.text = "マウスの左クリックで通常のテキストファイルの読み込み、読み込みしたテキストが表示されます。";
+		///dataText.text = "マウスの左クリックで改行ごとに分割した文章が表示されます。";
 	}
 
-	void Update()
+/*	void Update()
 	{
 		//　読み込んだテキストファイルの内容を表示
 		if (Input.GetButtonDown("Fire1"))
@@ -50,5 +50,5 @@ public class LoadText : MonoBehaviour
 			}
 			//　Resourcesフォルダに配置したテキストファイルの内容を表示
 		}
-	}
+	}*/
 }
